97cbe0382ff107094486914f82e875d8d50165cd
Lucas Santos <lucaskfsantos@gmail.com>
2025-01-05 13:58:00 -0300

    feature(welcome page): construção da página inicial

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WhatsApp Clone',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark(),
       home: WelcomePage(),
     );
   }
