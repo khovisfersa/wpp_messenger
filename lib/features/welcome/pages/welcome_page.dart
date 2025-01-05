d5427aad9d4b402402b7a4f2f98926899cf73a13
Lucas Santos <lucaskfsantos@gmail.com>
2025-01-05 17:57:28 -0300

    feature(welcome page): atualização da página de boas vindas
    Signed-off-by: Lucas Santos <lucaskfsantos@gmail.com>

--- lib/features/welcome/pages/welcome_page.dart
+++ lib/features/welcome/pages/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wpp_messenger/common/utils/coloors.dart';
+import 'package:wpp_messenger/common/utils/custom_elevated_button.dart';
+import 'package:wpp_messenger/common/utils/language_button.dart';
+import 'package:wpp_messenger/widgets/privacy_and_terms.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,7 +10,7 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF111B21),
+      backgroundColor: Coloors.backgroundGrey,
       body: Center(
         child: Column(
           children: [
@@ -26,73 +30,24 @@ class WelcomePage extends StatelessWidget {
                       text: const TextSpan(
                           text: "Read our: ",
                           style:
-                              TextStyle(color: Color(0xFF8696A0), height: 1.5),
+                              TextStyle(color: Coloors.darkGrey, height: 1.5),
                           children: [
                             TextSpan(
                                 text: "Privacy policy ",
-                                style: TextStyle(color: Color(0xFF4400f0))),
+                                style: TextStyle(color: Coloors.darkBlue)),
                             TextSpan(
                                 text: 'tap "Accept and agree" to accept the '),
                             TextSpan(
                                 text: "Terms of Service",
-                                style: TextStyle(color: Color(0xFF4400f0)))
+                                style: TextStyle(color: Coloors.darkBlue))
                           ])),
                 ),
-                SizedBox(
-                  height: 42,
-                  width: MediaQuery.of(context).size.width - 100,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF00A884),
-                        foregroundColor: Color(0xFF111B21),
-                        splashFactory: NoSplash.splashFactory,
-                        elevation: 0,
-                        shadowColor: Colors.transparent,
-                      ),
-                      child: const Text("AGREE AND ACCEPT")),
-                ),
+                // const PrivacyAndTerms(),
+                Customelevatedbutton(buttonText: "AGREE AND ACCEPT", onPressed: (){}),
                 const SizedBox(
                   height: 50,
                 ),
-                Material(
-                  color: const Color(0xFF182222),
-                  borderRadius: BorderRadius.circular(20),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Center(
-                      child: InkWell(
-                        onTap: () {},
-                        borderRadius: BorderRadius.circular(20),
-                        splashFactory: NoSplash.splashFactory,
-                        highlightColor: const Color(0xFF09141A),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.language,
-                              color: Color(0xFF00A884),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "English",
-                              style: TextStyle(),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Icon(
-                              Icons.keyboard_arrow_down,
-                              color: Color(0xFF00A884),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                LanguageButton(language: "English")
               ],
             ))
           ],
